5fd876bcd86c4f0f7bb71d9fffde7d44b1a4d020
Tacce <francescotaccetti@hotmail.com>
2026-01-23 23:14:09 +0100

    Black Artifacts removed. Illumination correction added (commented).

--- AR_F2R.py
+++ AR_F2R.py
@@ -13,10 +13,11 @@ out = cv2.VideoWriter('output.avi', fourcc, fps, (w_aug,  h_aug))
 
 ref_frame = cv2.imread('ReferenceFrame.png')
 h_frame, w_frame = ref_frame.shape[0], ref_frame.shape[1]
-object_mask = cv2.imread('ObjectMask.PNG',cv2.IMREAD_GRAYSCALE).astype(bool)
+object_mask = cv2.imread('ObjectMask.PNG',cv2.IMREAD_GRAYSCALE)
 
 ref_frame_masked = ref_frame.copy()
-ref_frame_masked[~object_mask] = 0
+ref_frame_masked[object_mask==0] = 0
+# ref_frame_intensity = np.array(cv2.mean(ref_frame, mask=object_mask)[:3])
 
 aug_layer = cv2.imread('AugmentedLayer.PNG')
 aug_layer = aug_layer[:h_frame, :w_frame]
@@ -28,11 +29,6 @@ H = np.eye(3)
 # SIFT detector
 sift = cv2.SIFT_create()
 kp_rf = sift.detect(ref_frame_masked)
-'''
-img_visualization = cv2.drawKeypoints(cv2.cvtColor(ref_frame_masked, cv2.COLOR_BGR2RGB),kp_rf,None,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-plt.imshow(img_visualization)
-plt.show()
-'''
 kp_rf, des_rf = sift.compute(ref_frame_masked, kp_rf)
 
 while cap.isOpened():
@@ -61,11 +57,17 @@ while cap.isOpened():
     dst_pts = np.float32([kp_frame[m.trainIdx].pt for m in good]).reshape(-1,1,2)
     M, match_mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
     
-    warped = cv2.warpPerspective(aug_layer, M, (w_frame, h_frame), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
-    warp_mask = cv2.warpPerspective(aug_mask, M, (w_frame, h_frame), flags=cv2.INTER_NEAREST, borderMode=cv2.BORDER_CONSTANT)
-    
-    warp_mask = warp_mask == 0
+    warped = cv2.warpPerspective(aug_layer, M, (w_frame, h_frame), flags=cv2.INTER_LINEAR)
+    warp_mask = cv2.warpPerspective(aug_mask, M, (w_frame, h_frame), flags=cv2.INTER_LINEAR) < 250
 
+    '''
+    # Illumination correction
+    warp_object_mask = cv2.warpPerspective(object_mask, M, (w_frame, h_frame), flags=cv2.INTER_LINEAR) > 250
+    frame_intensity = np.array(cv2.mean(frame, mask=warp_object_mask.astype(np.uint8))[:3])
+
+    light_correction = frame_intensity / ref_frame_intensity
+    warped = np.clip(warped * light_correction, 0, 255).astype(np.uint8)
+    '''
     warped[warp_mask] = frame[warp_mask]
     
     out.write(warped)
